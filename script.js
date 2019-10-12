2bf176199cb89e863326cf7e12b19a25df3115d6
Chase Larson <bchaselarson@gmail.com>
2019-10-12 01:47:33 +0000

    major changes

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 /*global fetch*/
-document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+document.getElementById("nameSubmit").addEventListener("click", function(event) {
     event.preventDefault();
-    const value = document.getElementById("weatherInput").value;
+    const value = document.getElementById("nameInput").value;
     if (value === "")
         return;
     console.log(value);
     const url = "https://api.genderize.io/?name=" + value;
-    //let results = "<h2>" + url + "</h2>";
     fetch(url)
         .then(function(response) {
             return response.json();
@@ -20,10 +19,10 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
             results += "<h2>Probability of " + json.name + " being " + json.gender + "*: " + percent + "%</h2>";
             results += "<h2>Count**: " + json.count + "</h2>";
             
-    document.getElementById("weatherResults").innerHTML = results;
+    document.getElementById("nameResults").innerHTML = results;
     });
     
     let footnotes = "<h3>*Probability indicates the ratio of males to females</h3>" + "<h3>**Count represents the number of data rows examined in order to calculate the response</h3>";
-    document.getElementById("forecastResults").innerHTML = footnotes;
+    document.getElementById("footnotes").innerHTML = footnotes;
     
 });
